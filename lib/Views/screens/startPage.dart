2febadb93501f6f379f398cc04a9ae7bac629936
adnansaky <adnansaky123@gmail.com>
2023-02-04 02:08:57 +0600

    Update startPage.dart

--- lib/Views/screens/startPage.dart
+++ lib/Views/screens/startPage.dart
@@ -5,10 +5,47 @@ class StartPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
     return Center(
       child: Column(
         children: [
-          Image.asset('assets/logo/logo.png')
+          const Padding(padding: EdgeInsets.fromLTRB(0, 165, 0, 0)),
+          Image.asset(
+            'assets/logo/logo.png',
+          ),
+          const SizedBox(
+            height: 50.0,
+          ),
+          const Text(
+            'Bringin',
+            style: TextStyle(
+              decoration: TextDecoration.none,
+              fontSize: 35,
+              color: Colors.black,
+            ),
+          ),
+          const SizedBox(
+            height: 30.0,
+          ),
+          const Text(
+            'Instant Chat, Hire Direct',
+            style: TextStyle(
+              decoration: TextDecoration.none,
+              fontSize: 20,
+              color: Colors.black,
+            ),
+          ),
+          const SizedBox(
+            height: 300,
+          ),
+          const Text(
+            'Bringin Technologies Ltd.',
+            style: TextStyle(
+              decoration: TextDecoration.none,
+              fontSize: 20,
+              color: Colors.black,
+            ),
+          ),
         ],
       ),
     );
