6b7f6a9ceee8a14958a873a94e60c245167a6388
adnansaky <adnansaky123@gmail.com>
2023-02-04 00:55:04 +0600

    Create pageRoute.dart

---
+++ lib/helper/pageRoute.dart
@@ -0,0 +1,13 @@
+import 'package:get/get_navigation/src/routes/get_route.dart';
+
+
+class page{
+  static List<GetPage<dynamic>> pages() {
+    return[
+      // GetPage(name: '/', page: () => OTPPage()),
+      // GetPage(name: '/second', page: () => VerifyOTPPage()),
+      // GetPage(name: '/third', page: () => UserProfilePage()),
+    ];
+  }
+
+}
