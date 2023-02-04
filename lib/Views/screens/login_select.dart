64ea0551c0f4097fc9a2eb96052f891461d33ecb
adnansaky <adnansaky123@gmail.com>
2023-02-04 14:04:35 +0600

    Update login_select.dart

--- lib/Views/screens/login_select.dart
+++ lib/Views/screens/login_select.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'SignIn.dart';
 
 class LoginSelect extends StatelessWidget {
   const LoginSelect({Key? key}) : super(key: key);
@@ -8,13 +11,36 @@ class LoginSelect extends StatelessWidget {
     return Column(
       children: [
         const Padding(padding: EdgeInsets.fromLTRB(0, 41, 0, 20)),
+        const Text(
+          'Choose Your Role',
+          style: TextStyle(
+            decoration: TextDecoration.none,
+            fontSize: 23,
+            color: Colors.black,
+          ),
+        ),
+        const Padding(padding: EdgeInsets.fromLTRB(0, 41, 0, 20)),
         Image.asset('assets/img/img2.png'),
         const Padding(padding: EdgeInsets.fromLTRB(0, 20, 0, 20)),
-        ElevatedButton(onPressed: (){}, child: const Text("Join as Job Seeker")),
+        ElevatedButton(
+            onPressed: () {
+
+              Get.to(SignIn());
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.greenAccent,
+            ),
+            child: const Text("Join as Job Seeker")),
         const Padding(padding: EdgeInsets.fromLTRB(0, 40, 0, 40)),
         Image.asset('assets/img/img1.png'),
         const Padding(padding: EdgeInsets.fromLTRB(0, 20, 0, 20)),
-        ElevatedButton(onPressed: (){}, child: const Text("Join as Recruiter")),
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.greenAccent,
+          ),
+          child: const Text("Join as Recruiter"),
+        ),
       ],
     );
   }
