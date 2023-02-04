908ba145c56531d42bd39e0c7f3c2441e7bf16d6
adnansaky <adnansaky123@gmail.com>
2023-02-04 12:38:06 +0600

    Update login_select.dart

--- lib/Views/screens/login_select.dart
+++ lib/Views/screens/login_select.dart
@@ -5,6 +5,17 @@ class LoginSelect extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Column(
+        children: [
+          Padding(padding: EdgeInsets.fromLTRB(0, 41, 0, 20)),
+          Image.asset('asstes/img/img1.png'),
+          Padding(padding: EdgeInsets.fromLTRB(0, 20, 0, 20)),
+          ElevatedButton(onPressed: (){}, child: Text("Join as Job Seeker")),
+          Padding(padding: EdgeInsets.fromLTRB(0, 40, 0, 40)),
+          Image.asset('asstes/img/img2.png'),
+        ],
+      ),
+    );
   }
 }
