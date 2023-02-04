b10c20e5c134d0293c3e967f31013f9f8f434a24
adnansaky <adnansaky123@gmail.com>
2023-02-04 17:25:37 +0600

    Update SignIn.dart

--- lib/Views/screens/SignIn.dart
+++ lib/Views/screens/SignIn.dart
@@ -1,24 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
 
 class SignIn extends StatelessWidget {
   const SignIn({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: [
-          Padding(padding: EdgeInsets.fromLTRB(0, 150 ,  0, 30)),
-          const Text(
-            'Sign In / Sign Up\nJob Seeker ',
-            style: TextStyle(
-              decoration: TextDecoration.none,
-              fontSize: 28,
-              color: Colors.black,
+
+    return Scaffold(
+      body: Center(
+            child: Column(
+          children:  [
+            const Padding(padding: EdgeInsets.fromLTRB(0, 50, 0, 30)),
+            const Text(
+              'Sign In / Sign Up \nJob Seeker',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                decoration: TextDecoration.none,
+                fontSize: 28,
+                color: Colors.black,
+              ),
             ),
-          ),
-        ],
-      ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+              child: Text(
+                'If you already have account you will be log in to your profile. '
+                'Otherwise you will be given the option to set a new profile.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  decoration: TextDecoration.none,
+                  fontSize: 14,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            Padding(padding: const EdgeInsets.all(20),
+              child: IntlPhoneField(
+                initialCountryCode: 'BD',
+                decoration: const InputDecoration(
+                  labelText: 'Phone Number',
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(),
+                  ),
+                ),
+              ),
+            ),
+            
+          ],
+        )),
+
     );
   }
 }
