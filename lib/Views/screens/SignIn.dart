04d972205559c1a2c06d3177dbd98e0a749f42bd
adnansaky <adnansaky123@gmail.com>
2023-02-04 18:25:49 +0600

    Update SignIn.dart

--- lib/Views/screens/SignIn.dart
+++ lib/Views/screens/SignIn.dart
@@ -1,4 +1,6 @@
+import 'package:bringin/Views/screens/OTPSent.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
 
 class SignIn extends StatelessWidget {
@@ -6,49 +8,58 @@ class SignIn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: Center(
-            child: Column(
-          children:  [
-            const Padding(padding: EdgeInsets.fromLTRB(0, 50, 0, 30)),
-            const Text(
-              'Sign In / Sign Up \nJob Seeker',
+          child: Column(
+        children: [
+          const Padding(padding: EdgeInsets.fromLTRB(0, 150, 0, 30)),
+          const Text(
+            'Sign In / Sign Up \nJob Seeker',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontWeight: FontWeight.w900,
+              decoration: TextDecoration.none,
+              fontSize: 28,
+              color: Colors.black,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+            child: Text(
+              'If you already have account you will be log in to your profile. '
+              'Otherwise you will be given the option to set a new profile.',
               textAlign: TextAlign.center,
               style: TextStyle(
                 decoration: TextDecoration.none,
-                fontSize: 28,
+                fontSize: 14,
                 color: Colors.black,
               ),
             ),
-            const Padding(
-              padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-              child: Text(
-                'If you already have account you will be log in to your profile. '
-                'Otherwise you will be given the option to set a new profile.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  decoration: TextDecoration.none,
-                  fontSize: 14,
-                  color: Colors.black,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: IntlPhoneField(
+              initialCountryCode: 'BD',
+              decoration: const InputDecoration(
+                labelText: 'Phone Number',
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(),
                 ),
               ),
             ),
-            Padding(padding: const EdgeInsets.all(20),
-              child: IntlPhoneField(
-                initialCountryCode: 'BD',
-                decoration: const InputDecoration(
-                  labelText: 'Phone Number',
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(),
-                  ),
-                ),
-              ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(0),
+            child: ElevatedButton(
+              onPressed: () {
+                Get.to(()=>OTPSent());
+              },
+              child: Text('Send OTP'),
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.white70),
             ),
-            
-          ],
-        )),
-
+          )
+        ],
+      )),
     );
   }
 }
