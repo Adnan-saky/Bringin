6f291e7034abe5e2173479f940d1b060e4ec0ced
adnansaky <adnansaky123@gmail.com>
2023-02-04 19:59:41 +0600

    Update login_select.dart

--- lib/Views/screens/login_select.dart
+++ lib/Views/screens/login_select.dart
@@ -8,40 +8,46 @@ class LoginSelect extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Padding(padding: EdgeInsets.fromLTRB(0, 41, 0, 20)),
-        const Text(
-          'Choose Your Role',
-          style: TextStyle(
-            decoration: TextDecoration.none,
-            fontSize: 23,
-            color: Colors.black,
-          ),
-        ),
-        const Padding(padding: EdgeInsets.fromLTRB(0, 41, 0, 20)),
-        Image.asset('assets/img/img2.png'),
-        const Padding(padding: EdgeInsets.fromLTRB(0, 20, 0, 20)),
-        ElevatedButton(
-            onPressed: () {
-
-              Get.to(() => SignIn());
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.greenAccent,
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Padding(padding: EdgeInsets.fromLTRB(0, 41, 0, 0)),
+            const Text(
+              'Choose Your Role',
+              style: TextStyle(
+                decoration: TextDecoration.none,
+                fontSize: 30,
+                fontWeight: FontWeight.w800,
+                color: Colors.black,
+              ),
             ),
-            child: const Text("Join as Job Seeker")),
-        const Padding(padding: EdgeInsets.fromLTRB(0, 40, 0, 40)),
-        Image.asset('assets/img/img1.png'),
-        const Padding(padding: EdgeInsets.fromLTRB(0, 20, 0, 20)),
-        ElevatedButton(
-          onPressed: () {},
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.greenAccent,
-          ),
-          child: const Text("Join as Recruiter"),
+            const Padding(padding: EdgeInsets.fromLTRB(0, 41, 0, 20)),
+            Image.asset('assets/img/img2.png'),
+            const Padding(padding: EdgeInsets.fromLTRB(0, 20, 0, 0)),
+            ElevatedButton(
+                onPressed: () {
+
+                  Get.to(() => SignIn());
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.greenAccent,
+                ),
+                child: const Text("Join as Job Seeker")),
+            const Padding(padding: EdgeInsets.fromLTRB(0, 40, 0, 0)),
+            Image.asset('assets/img/img1.png'),
+            const Padding(padding: EdgeInsets.fromLTRB(0, 20, 0, 0)),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.greenAccent,
+              ),
+              child: const Text("Join as Recruiter"),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
