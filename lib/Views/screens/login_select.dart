aca886be1863b855d6f9fc6075811f4f3fabd9c4
adnansaky <adnansaky123@gmail.com>
2023-02-05 15:37:57 +0600

    Update login_select.dart

--- lib/Views/screens/login_select.dart
+++ lib/Views/screens/login_select.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../Models/SendOTPModel.dart';
+import '../widgets/customText.dart';
 import 'SignIn.dart';
 
 class LoginSelect extends StatelessWidget {
@@ -8,44 +10,60 @@ class LoginSelect extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Padding(padding: EdgeInsets.fromLTRB(0, 41, 0, 0)),
-            const Text(
-              'Choose Your Role',
-              style: TextStyle(
-                decoration: TextDecoration.none,
-                fontSize: 30,
-                fontWeight: FontWeight.w800,
-                color: Colors.black,
-              ),
-            ),
-            const Padding(padding: EdgeInsets.fromLTRB(0, 41, 0, 20)),
-            Image.asset('assets/img/img2.png'),
-            const Padding(padding: EdgeInsets.fromLTRB(0, 20, 0, 0)),
-            ElevatedButton(
-                onPressed: () {
+    String role;
 
-                  Get.to(() => SignIn());
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.greenAccent,
+    return Scaffold(
+      body: SingleChildScrollView(
+        physics : const ScrollPhysics(),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 41, 0, 0),
+                child: CustomText(
+                  text: 'Choose Your Role',
+                  fontSize: 30,
                 ),
-                child: const Text("Join as Job Seeker")),
-            const Padding(padding: EdgeInsets.fromLTRB(0, 40, 0, 0)),
-            Image.asset('assets/img/img1.png'),
-            const Padding(padding: EdgeInsets.fromLTRB(0, 20, 0, 0)),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.greenAccent,
               ),
-              child: const Text("Join as Recruiter"),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 41, 0, 20),
+                child: Image.asset('assets/img/img2.png'),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                child: ElevatedButton(
+                    onPressed: () {
+                      role = 'seekers';
+
+                      print(role);
+
+                      Get.to(() => SignIn());
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.greenAccent,
+                    ),
+                    child: const Text("Join as Job Seeker")),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+                child: Image.asset('assets/img/img1.png'),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                child: ElevatedButton(
+                  onPressed: () {
+                    role  = "recruiters";
+                    print(role);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.greenAccent,
+                  ),
+                  child: const Text("Join as Recruiter"),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
