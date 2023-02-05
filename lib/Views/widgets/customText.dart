2a939ecd269788eee5ca4cf72b04488f11d2f2f7
adnansaky <adnansaky123@gmail.com>
2023-02-05 15:38:02 +0600

    Create customText.dart

---
+++ lib/Views/widgets/customText.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CustomText extends StatelessWidget {
+  final String text;
+  final double fontSize;
+
+  CustomText({required this.text, this.fontSize = 30});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        decoration: TextDecoration.none,
+        fontSize: fontSize,
+        fontWeight: FontWeight.w800,
+        color: Colors.black,
+      ),
+    );
+  }
+}
