cc432400050e064f757e27ce33fa43eb3bc16540
adnansaky <adnansaky123@gmail.com>
2023-02-06 19:10:12 +0600

    Update EditProfile.dart

--- lib/Views/screens/EditProfile.dart
+++ lib/Views/screens/EditProfile.dart
@@ -8,36 +8,33 @@ import 'package:get/get.dart';
 
 import '../widgets/imgpicker.dart';
 
-class EditProfile extends StatefulWidget {
-  @override
-  _EditProfileState createState() => _EditProfileState();
-}
+class EditProfile extends StatelessWidget {
 
-class _EditProfileState extends State<EditProfile> {
-  final _formKey = GlobalKey<FormState>();
-  final _firstNameController = TextEditingController();
-  final _lastNameController = TextEditingController();
-  final _genderController = TextEditingController();
-  final _photoController = TextEditingController();
-  final _portfolioURLController = TextEditingController();
-  final _educationLevelController = TextEditingController();
-  final _DOBController = TextEditingController();
-  final _bioController = TextEditingController();
-  final _isExperienceController = TextEditingController();
-  final _skillsController = TextEditingController();
-  final _greetingController = TextEditingController();
-  bool _loading = false;
-
-
+  final String token;
+  EditProfile({required this.token});
+  final File selectedImage  = File('');
+  final firstNameController = TextEditingController().obs;
+  final lastNameController = TextEditingController().obs;
+  final genderController = TextEditingController().obs;
+  final photoController = TextEditingController().obs;
+  final portfolioURLController = TextEditingController().obs;
+  final educationLevelController = TextEditingController().obs;
+  final DOBController = TextEditingController().obs;
+  final bioController = TextEditingController().obs;
+  final isExperienceController = TextEditingController().obs;
+  final skillsController = TextEditingController().obs;
+  final greetingController = TextEditingController().obs;
+  final formKey = GlobalKey<FormState>();
+  final loading = false.obs;
 
   Future<void> updateProfile() async {
-    if (_formKey.currentState!.validate()) {
-      setState(() {
-        _loading = true;
-      });
+    print("asdasdasdasdasdsadasdsadasdwebwasdaeweggsdgsdgsdg");
 
-      try {
-        var uri = Uri.tryParse("https://bringin.io/api/seekerProfileUpdate");
+      loading.value = true;
+      print(token);
+
+
+        var uri = Uri.tryParse('https://bringin.io/api/seekerProfileUpdate');
         if (uri == null) {
           throw Exception("Invalid URL");
         }
@@ -46,25 +43,26 @@ class _EditProfileState extends State<EditProfile> {
           uri,
           headers: {
             'Content-Type': 'application/json',
+            'Authorization': 'Bearer $token',
           },
           body: jsonEncode({
-            'firstName': _firstNameController.text,
-            'lastName': _lastNameController.text,
-            'gender': _genderController.text,
-            'photo': _photoController.text,
-            'portfolioURL': _portfolioURLController.text,
-            'educationLevel': _educationLevelController.text,
-            'DOB': _DOBController.text,
-            'bio': _bioController.text,
-            'isExperience': _isExperienceController.text,
-            'skills': _skillsController.text,
-            'greeting': _greetingController.text,
+            'firstName': firstNameController.value.text,
+            'lastName': lastNameController.value.text,
+            'gender': genderController.value.text,
+            'photo': photoController.value.text,
+            'portfolioURL': portfolioURLController.value.text,
+            'educationLevel': educationLevelController.value.text,
+            'DOB': DOBController.value.text,
+            'bio': bioController.value.text,
+            'isExperience': isExperienceController.value.text,
+            'skills': skillsController.value.text,
+            'greeting': greetingController.value.text,
           }),
         );
+        print("asdasdasdasdasdsadasdsadasdwebwasdaeweggsdgsdgsdg");
+        print(response.statusCode);
 
-        setState(() {
-          _loading = false;
-        });
+        loading.value = false;
         print(response.body);
         if (response.statusCode == 201) {
           final data = jsonDecode(response.body);
@@ -81,189 +79,192 @@ class _EditProfileState extends State<EditProfile> {
             duration: Duration(seconds: 2),
           );
         }
-      } catch (e) {
-        setState(() {
-          _loading = false;
-        });
-        Get.snackbar(
-          'Error',
-          'Failed to update profile',
-          duration: Duration(seconds: 2),
+      // } catch (e) {
+      //   loading.value = false;
+      //   Get.snackbar(
+      //     'Error',
+      //     'Failed to update profile',
+      //     duration: Duration(seconds: 2),
+      //   );
+      // }
+      loading.value = false;
+
+  }
+
+
+      @override
+      Widget build(BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("Update Profile"),
+          ),
+          body: loading.value
+              ? Center(
+            child: CircularProgressIndicator(),
+          )
+              : SingleChildScrollView(
+            child: Form(
+              key: formKey,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    (CircularImagePicker()),
+                    TextFormField(
+                      controller: firstNameController.value,
+                      decoration: InputDecoration(
+                        labelText: "First Name",
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Please enter your first name";
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: lastNameController.value,
+                      decoration: InputDecoration(
+                        labelText: "Last Name",
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Please enter your last name";
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: genderController.value,
+                      decoration: InputDecoration(
+                        labelText: "Gender",
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Please enter your gender";
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: photoController.value,
+                      decoration: InputDecoration(
+                        labelText: "Photo URL",
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Please enter your photo URL";
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: portfolioURLController.value,
+                      decoration: InputDecoration(
+                        labelText: "Portfolio URL",
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Please enter your portfolio URL";
+                        }
+                        return null;
+                      },
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: TextFormField(
+                        controller: educationLevelController.value,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "Education Level",
+                          //hintText: 'Enter Your Education Level',
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Please enter your education level";
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    TextFormField(
+                      controller: DOBController.value,
+                      decoration: InputDecoration(
+                        labelText: "Date of Birth",
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Please enter your date of birth";
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: bioController.value,
+                      decoration: InputDecoration(
+                        labelText: "Bio",
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Please enter your bio";
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: isExperienceController.value,
+                      decoration: InputDecoration(
+                        labelText: "experence",
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Please enter your bio";
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: skillsController.value,
+                      decoration: InputDecoration(
+                        labelText: "Skills",
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Please enter your bio";
+                        }
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      controller: greetingController.value,
+                      decoration: InputDecoration(
+                        labelText: "greeting",
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Please enter your bio";
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    ElevatedButton(
+                      onPressed: () => updateProfile(),
+                      child: Text("Update Profile"),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         );
       }
     }
-  }
 
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Update Profile"),
-      ),
-      body: _loading
-          ? Center(
-        child: CircularProgressIndicator(),
-      )
-          : SingleChildScrollView(
-            child: Form(
-        key: _formKey,
-        child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                (CircularImagePicker()),
-                TextFormField(
-                  controller: _firstNameController,
-                  decoration: InputDecoration(
-                    labelText: "First Name",
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "Please enter your first name";
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _lastNameController,
-                  decoration: InputDecoration(
-                    labelText: "Last Name",
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "Please enter your last name";
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _genderController,
-                  decoration: InputDecoration(
-                    labelText: "Gender",
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "Please enter your gender";
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _photoController,
-                  decoration: InputDecoration(
-                    labelText: "Photo URL",
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "Please enter your photo URL";
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _portfolioURLController,
-                  decoration: InputDecoration(
-                    labelText: "Portfolio URL",
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "Please enter your portfolio URL";
-                    }
-                    return null;
-                  },
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: TextFormField(
-                    controller: _educationLevelController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "Education Level",
-                      //hintText: 'Enter Your Education Level',
-                    ),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return "Please enter your education level";
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-                TextFormField(
-                  controller: _DOBController,
-                  decoration: InputDecoration(
-                    labelText: "Date of Birth",
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "Please enter your date of birth";
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _bioController,
-                  decoration: InputDecoration(
-                    labelText: "Bio",
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "Please enter your bio";
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _isExperienceController,
-                  decoration: InputDecoration(
-                    labelText: "experence",
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "Please enter your bio";
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _skillsController,
-                  decoration: InputDecoration(
-                    labelText: "Skills",
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "Please enter your bio";
-                    }
-                    return null;
-                  },
-                ),
-                TextFormField(
-                  controller: _greetingController,
-                  decoration: InputDecoration(
-                    labelText: "greeting",
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "Please enter your bio";
-                    }
-                    return null;
-                  },
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                ElevatedButton(
-                  onPressed:  updateProfile,
-                  child: Text("Update Profile"),
-                ),
-              ],
-            ),
-        ),
-      ),
-          ),
-    );
-  }
-}
+
+
 
