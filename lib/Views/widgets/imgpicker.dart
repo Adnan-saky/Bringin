6e0e776437692f5fd2fa19e9f09c73ac16a43b3b
adnansaky <adnansaky123@gmail.com>
2023-02-06 19:46:56 +0600

    Update imgpicker.dart

--- lib/Views/widgets/imgpicker.dart
+++ lib/Views/widgets/imgpicker.dart
@@ -1,22 +1,24 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 
-class CircularImagePicker extends StatefulWidget {
-  @override
-  _CircularImagePickerState createState() => _CircularImagePickerState();
+class CircularImagePickerController extends GetxController {
+  File? _image;
+  ImagePicker imagePicker = ImagePicker();
+
+  Future<void> onPictureSelection() async {
+    var image  = await imagePicker.pickImage(source: ImageSource.gallery);
+    if (image  == null) return;
+    final imageTemporary = File(image.path);
+    _image = imageTemporary;
+    print(_image.toString());
+    update();
+  }
 }
 
-class _CircularImagePickerState extends State<CircularImagePicker> {
-  var _selectedImage;
-  XFile ? image;
-  ImagePicker imagePicker = ImagePicker();
-  Future<void> _onPictureSelection() async {
-    var image = await imagePicker.pickImage(source: ImageSource.gallery);
-    setState(() {
-      _selectedImage = File(image!.path);
-    });
-  }
+class CircularImagePicker extends StatelessWidget {
+  final CircularImagePickerController controller = Get.put(CircularImagePickerController());
 
   @override
   Widget build(BuildContext context) {
@@ -24,15 +26,15 @@ class _CircularImagePickerState extends State<CircularImagePicker> {
       height: 150,
       width: 150,
       child: GestureDetector(
-        onTap: _onPictureSelection,
+        onTap: controller.onPictureSelection,
         child: CircleAvatar(
           backgroundColor: Colors.grey[200],
           radius: 75,
-          child: _selectedImage == null
+          child: controller._image == null
               ? Icon(Icons.add_a_photo, size: 50)
               : ClipOval(
             child: Image.file(
-              _selectedImage! ,
+              controller._image!,
               fit: BoxFit.cover,
               width: 150,
               height: 150,
