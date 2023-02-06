fbc8d628272f7db2d1349ead7f04b0c72964b7ad
adnansaky <adnansaky123@gmail.com>
2023-02-06 21:15:28 +0600

    Update imgpicker.dart

--- lib/Views/widgets/imgpicker.dart
+++ lib/Views/widgets/imgpicker.dart
@@ -1,47 +1,53 @@
-import 'dart:io';
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:image_picker/image_picker.dart';
-
-class CircularImagePickerController extends GetxController {
-  File? _image;
-  ImagePicker imagePicker = ImagePicker();
-
-  Future<void> onPictureSelection() async {
-    var image  = await imagePicker.pickImage(source: ImageSource.gallery);
-    if (image  == null) return;
-    final imageTemporary = File(image.path);
-    _image = imageTemporary;
-    print(_image.toString());
-    update();
-  }
-}
-
-class CircularImagePicker extends StatelessWidget {
-  final CircularImagePickerController controller = Get.put(CircularImagePickerController());
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 150,
-      width: 150,
-      child: GestureDetector(
-        onTap: controller.onPictureSelection,
-        child: CircleAvatar(
-          backgroundColor: Colors.grey[200],
-          radius: 75,
-          child: controller._image == null
-              ? Icon(Icons.add_a_photo, size: 50)
-              : ClipOval(
-            child: Image.file(
-              controller._image!,
-              fit: BoxFit.cover,
-              width: 150,
-              height: 150,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
+// import 'dart:io';
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:image_picker/image_picker.dart';
+//
+// class CircularImagePickerController extends GetxController {
+//
+//
+//   //  var path = ''.obs;
+//   // File? _image;
+//   // ImagePicker imagePicker = ImagePicker();
+//   //
+//   // Future<void> onPictureSelection() async {
+//   //   final image  = await imagePicker.pickImage(source: ImageSource.gallery);
+//   //   if (image  == null) return;
+//   //
+//   //   _image =  File(image.path);
+//   //   path.value = _image!.path;
+//   //   print(path.value);
+//   //   print(_image.toString());
+//   //   //update();
+//   // }
+// }
+//
+// class CircularImagePicker extends StatelessWidget {
+//   final CircularImagePickerController controller = Get.put(CircularImagePickerController());
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       height: 150,
+//       width: 150,
+//       child: GestureDetector(
+//         onTap: onPictureSelection,
+//         child: CircleAvatar(
+//           backgroundColor: Colors.grey[200],
+//           radius: 75,
+//           child:
+//             // controller.path.value == null
+//           //     ? Icon(Icons.add_a_photo, size: 50)
+//           //     :
+//              CircleAvatar(
+//
+//             backgroundImage: FileImage(File(controller.path.value)),
+//             radius: 80,
+//           )
+//
+//             ),
+//           )
+//
+//     );
+//   }
+// }
